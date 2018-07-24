8a5d975abf7c0122653c82cd3205b6c537d40a4c
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-07-24 02:27:52 -0500

    fixing android file downloader. just saves to Download/. imagine that. closes turtl/tracker#109

--- www/download.js
+++ www/download.js
@@ -1,16 +1,3 @@
-var blob_to_arraybuffer = function(blob)
-{
-	return new Promise(function(resolve, reject) {
-		var reader = new FileReader();
-		reader.onload = function() {
-			resolve(this.result);
-		};
-		reader.onerror = reject;
-		reader.onabort = reject;
-		reader.readAsArrayBuffer(blob);
-	});
-};
-
 /**
  * NOTE: here, we're replacing a function definition from the app, such that
  * files the app is meant to download will be passed here instead.
@@ -22,36 +9,41 @@ var download_blob = function(blob, options)
 	// -------------------------------------------------------------------------
 	// NOTE: this not only doesn't really work at all, it screws up the indexeddb
 	// database in the app. holding out for a better way to handle files.
-	barfr.barf('Viewing files on mobile is currently broken. We\'re working on a fix. Sorry for the trouble!');
-	return;
+	//barfr.barf('Viewing files on mobile is currently broken. We\'re working on a fix. Sorry for the trouble!');
+	//return;
 	// -------------------------------------------------------------------------
 
 	var url;
+	var name = options.name || 'download';
 	return new Promise(function(resolve, reject) {
-		var name = options.name || 'download';
 		log.info('file: start: ', name);
-		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(filesystem) {
-			filesystem.root.getFile(name, {create: true}, function(entry) {
-				var file_url = entry.toURL();
-				log.info('file: got fs/file: ', file_url);
-				blob_to_arraybuffer(blob)
-					.then(function(buffer) {
-						var arr = new Uint8Array(buffer);
-						entry.createWriter(function(writer) {
-							writer.onerror = reject;
-							writer.onwriteend = function(e)
-							{
-								log.info('download: complete', e);
-								resolve([file_url, name]);
-							};
-							writer.write(arr);
-						}, reject);
-					})
-					.catch(reject);
-			}, reject);
-		}, reject);
+		var rejecter = function(name) {
+			return function(err) {
+				err.step = name;
+				reject(err);
+			};
+		};
+		window.resolveLocalFileSystemURL('file:///storage/emulated/0/', function(filesystem) {
+			filesystem.getDirectory('Download', {create: true, exclusive: false}, function(dir) {
+				dir.getFile(name, {create: true, exclusive: false}, function(entry) {
+					var file_url = entry.toURL();
+					entry.createWriter(function(writer) {
+						writer.onerror = rejecter('write error');
+						writer.onwriteend = function(e) {
+							log.info('download: complete', e);
+							resolve([file_url, name]);
+						};
+						writer.write(blob);
+					}, rejecter('create writer'));
+				}, rejecter('get file '+name));
+			}, rejecter('get Download/ directory entry'));
+		}, rejecter('resolve filesystem url'));
 	}).spread(function(file_url, name) {
-		window.open(file_url);
+		barfr.barf(i18next.t('File saved to Download/{{filename}}', {filename: name}));
+	}).catch(function(err) {
+		barfr.barf(i18next.t('There was a problem saving the file {{filename}}.', {filename: name}));
+		log.error('Problem saving file: ', err.step, err);
+		throw err;
 	}).finally(function() {
 		if(url) URL.revokeObjectURL(url);
 	});
