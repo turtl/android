efc9d0604dc14a182b4e1e0e454ec5fd2212eded
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-10-22 20:36:11 -0700

    implement share to -> turtl fully (when logged in). need to deal with non-logged-in state.

--- www/share-to.js
+++ www/share-to.js
@@ -5,9 +5,59 @@ function setup_share_to() {
 		log.error('share_to::init() -- error: ', err);
 	};
 	const init_success = function() {
-		log.info('share_to::init() -- success!');
 		cordova.openwith.addHandler(function(intent) {
-			console.log('got share intent: ', intent);
+			if(intent.action != 'SEND') return;
+			var shared = intent.items[0];
+			var note = new Note();
+			var file = null;
+			var type = null;
+			var loader_promise = Promise.resolve();
+			var editor = new NotesEditController({
+				type: type,
+				skip_modal: true,
+			});
+			var note = editor.model;
+			note.unset('type');
+			editor.release();
+			if(shared.type == 'text') {
+				var text = shared.data;
+				// is it a url?
+				if(text.match(/^([a-z]+?:\/\/.)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/i)) {
+					type = 'link';
+					note.set({url: text});
+				} else {
+					type = 'text';
+					note.set({text: text});
+				}
+			} else if(shared.path) {
+				// i imagine stuff like that goes on all the time heh heh.
+				var filename = shared.path.split('/');
+				filename = filename[filename.length - 1];
+				loader_promise = new Promise(function(resolve) {
+					cordova.openwith.load(shared, function(base64_data, _item) {
+						type = 'file';
+						note.get('file').unset('cleared').set({
+							set: true,
+							name: filename,
+							type: shared.type,
+							size: atob(base64_data).length,
+							filedata: {
+								data: base64_data,
+							},
+						});
+						resolve();
+					});
+				});
+			}
+			turtl.show_loading_screen(true);
+			return loader_promise.finally(function() {
+				note.set({type: type});
+				var editor = new NotesEditController({
+					model: note,
+					type: type,
+				});
+				turtl.show_loading_screen(false);
+			});
 		});
 	};
 	cordova.openwith.init(init_success, init_fail);
